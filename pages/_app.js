26638f626940042f5f9e7574e57796ea6c2861e7
Nikita <vclan@mail.ru>
2021-07-22 20:38:46 +0300

    bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/App.scss';
+import '../styles/Navigation.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/test.js
@@ -0,0 +1,11 @@
+import Navigation from '../components/Navigation';
+
+const test = () => {
+	return (
+		<>
+			<Navigation />
+			<h1>!!!!!!!!!!!!!!!!!!!!</h1>
+		</>
+	);
+};
+export default test;
